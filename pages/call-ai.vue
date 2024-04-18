b9722a3c1c44c47c6f7515b79f4bd09974bd157c
mohit nimavat <mohitnimavatgh@gmail.com>
2024-04-18 19:23:24 +0530

    update the signup button

--- pages/call-ai.vue
+++ pages/call-ai.vue
@@ -40,7 +40,7 @@
         </div>
       </Modal>
     </div>  
-    <Tabs class="mt-3" :menuItems="menuItems" @change="handleMenuChange" />
+    <!-- <Tabs class="mt-3" :menuItems="menuItems" @change="handleMenuChange" /> -->
     <div class="mt-5">
       <NuxtPage />
     </div>
---
+++ pages/register.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="bg-white dark:bg-gray-900 h-screen mt-5">
+        <div class="w-3/5 mx-auto flex">
+                <div class="w-3/5">
+                    <p class="text-2xl font-normal text-gray-800 flex text-center">High quality Bootstrap template for your next web project.</p>
+                    <span class="text-sm text-gray-500 mt-2 flex justify-center">Start your <span class="bg-orange-200">&nbsp;14days free&nbsp;</span> trial</span>
+                    <ul class="mt-6">
+                        <li class="flex items-center mt-1.5" >
+                            <i class="fas fa-check mr-2 text-primary-500"></i>
+                            <span class="text-sm text-gray-500">There are many variations of passages of Lorem Ipsum available, in some form, by injected humour</span>
+                        </li>
+                        <li class="flex items-center mt-1.5" >
+                            <i class="fas fa-check mr-2 text-primary-500"></i>
+                            <span class="text-sm text-gray-500">There are many variations of passages of Lorem Ipsum available, in some form, by injected humour</span>
+                        </li>
+                    </ul>
+                    <div class="flex">
+                        <div class="1/2">
+                            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg" src="https://hencework.com/theme/jampack/compact/dist/img/slide3.jpg" alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div>
+                        </div>
+                        <div class="1/2">
+                            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg" src="https://hencework.com/theme/jampack/compact/dist/img/slide4.jpg" alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-2/5">
+                    Right Side (40%)
+                </div>
+            </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'loginLayout',
+    // middleware: ["is-authenticate"]
+});
+</script>
