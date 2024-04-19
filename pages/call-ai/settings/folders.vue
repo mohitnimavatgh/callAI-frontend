d17d51ff16608cf7c8d9e7627b532e11c7b34b39
Mohit Nimavat <mohit.nimavat@bigsteptech.com>
2024-04-19 10:40:59 +0530

    folder and quickQuestions api integration

--- pages/call-ai/settings/folders.vue
+++ pages/call-ai/settings/folders.vue
@@ -11,10 +11,10 @@
       </div>
   
       <div class="mt-3 flex justify-end">
-        <FormInput type="text" icon="fas fa-search" :placeholder="`Search Folders`" class="w-56 "/>
+        <FormInput type="text" icon="fas fa-search" :placeholder="`Search Folders`" @search="handleSearch" class="w-56 "/>
       </div>
       <div class="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">        
-        <div v-for="i in 30" :key="i" class="relative col-span-full sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
+        <div v-for="folderItem in foldersLists?.data" :key="folderItem" class="relative col-span-full sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
           <div class="relative w-full mt-2">            
             <img src="@/assets/image/folder.svg" alt="Folder" class="w-full h-auto">            
             <div class="absolute top-10 right-4 flex">
@@ -22,12 +22,13 @@
               <i class="text-sm text-gray-400 fas fa-pen cursor-pointer"></i>
             </div>            
             <div class="absolute bottom-4 left-4">
-              <p class="font-medium text-gray-500 text-sm">Mohit Nimavat's Folder</p>
+              <p class="font-medium text-gray-500 text-sm">{{ folderItem.name }}</p>
               <p class="font-medium text-gray-400 text-xs">Meetings: 20</p>
             </div>
           </div>
-        </div>
+        </div>   
       </div>
+      <Pagination v-if="foldersLists && foldersLists.total && foldersLists.per_page && foldersLists.total > foldersLists.per_page" class="mt-4 flex justify-end" :totalRecords="foldersLists.total" :currentPage="folderParams.page" :recordsPerPage="foldersLists.per_page" @pageChange="folderPageChange"/>
       <Modal :title="'Add Folder'" :show="ShowAddModal" @close="ShowAddModal = false">
         <div class="modal-content  p-4 md:p-5">
           <div class="col-span-2 mb-3">
@@ -61,43 +62,66 @@
   </template>
   
 <script setup lang="ts">
-import { useFolders } from "@/stores/user/folders";
-import { useVuelidate } from "@vuelidate/core";
-import { required, helpers } from "@vuelidate/validators";
-const ShowAddModal = ref(false);
-const folders = useFolders()
+  import { useFolders } from "@/stores/user/folders";
+  import { useVuelidate } from "@vuelidate/core";
+  import { required, helpers } from "@vuelidate/validators";
+  const ShowAddModal = ref(false);
+  const folders = useFolders()
+  const folderParams = { page: 1,search:'' }
+  const items = [
+        { value: "private", icon: 'fas fa-lock',labelText: "Private Access", description: "Restrict visibility to admins only, hiding it from team members." },
+        { value: "public", icon: 'fas fa-user-group',labelText: "Team Access", description: "Grant team members access, making the folder visible to all." },
+      ]
 
-const items = [
-      { value: "private", icon: 'fas fa-lock',labelText: "Private Access", description: "Restrict visibility to admins only, hiding it from team members." },
-      { value: "public", icon: 'fas fa-user-group',labelText: "Team Access", description: "Grant team members access, making the folder visible to all." },
-    ]
+  const foldersLists = computed(() => folders.folderPagination);
 
-const folder = ref({
-    name: '',
-    access_type: 'private',   
-})
+  const folder = ref({
+      name: '',
+      access_type: 'private',   
+  })
 
-const rules = {
-    folder: {
-        name: {
-          required: helpers.withMessage("The Name field is required", required),
-        },
-        access_type: {
-          required: helpers.withMessage("The Folder Access field is required", required),
-        }       
-    }
-}
-const v$ = useVuelidate(rules, {folder})
+  const rules = {
+      folder: {
+          name: {
+            required: helpers.withMessage("The Name field is required", required),
+          },
+          access_type: {
+            required: helpers.withMessage("The Folder Access field is required", required),
+          }       
+      }
+  }
+  const v$ = useVuelidate(rules, {folder})
 
-async function createFolder() {  
-    const result = await v$.value.$validate()
-    if (result) {
-      folders.create(folder.value).then((resp:any) => {
-            if(resp.success) {
-              ShowAddModal.value = false;
-            }
-        })
-    }
-}
-  </script>
+  const getFolder = () => {
+    folders.list(folderParams)
+  }
+
+  onMounted(async () => {
+    await nextTick();
+    await getFolder();
+  })
+
+  async function createFolder() {  
+      const result = await v$.value.$validate()
+      if (result) {
+        folders.create(folder.value).then((resp:any) => {
+              if(resp.success) {
+                getFolder();
+                ShowAddModal.value = false;
+              }
+          })
+      }
+  }
+
+  const handleSearch = (value) => {
+    folderParams.search = value  
+    getFolder();  
+  };
+
+  const folderPageChange = (page: any) => {
+    folderParams.page = page
+    getFolder()
+  };
+  
+</script>
   
---
+++ stores/user/quickQuestions.ts
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia'
+
+export const useQuickQuestions = defineStore('quickQuestions', {
+  state: () => ({
+    quickQuestions: null,
+  }),
+  actions: {
+    async create(quickQuestion: any) {
+      try {        
+        const response = await useAPI('/quick-question', {
+          method: 'post',
+          body: quickQuestion,
+        });
+        const responseData = response.data.value;
+        return responseData;
+      } catch (error) {
+        throw error;
+      }
+    },
+    async list(quickQuestion) {
+      try {      
+        const response = await useAPI('/quick-question', {
+            method: 'get',
+            params : quickQuestion
+        });
+        const responseData = response.data.value;       
+        this.quickQuestions = responseData.data           
+        return responseData;
+      } catch (error) {
+        throw error;
+      }
+    },
+    async update(quickQuestion: any) {
+      try {      
+        const response = await useAPI(`/quick-question/update/?id=${quickQuestion.id}`, {
+          method: 'PATCH',
+          body: quickQuestion,
+        });
+        const responseData = response.data.value;
+        return responseData;
+      } catch (error) {
+        throw error;
+      }
+    },
+
+    async delete(quickQuestion_id: any) {
+      try {      
+        const response = await useAPI(`/quick-question/${quickQuestion_id}`, {
+          method: 'DELETE'    
+        });
+        const responseData = response.data.value
+        return responseData;
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+  persist: {
+    storage: persistedState.localStorage,
+  },
+});
