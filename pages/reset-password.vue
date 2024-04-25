c776f1c2bed9bd6598cb10e2481361a74c18c4bc
Mohit Nimavat <mohit@gmail.com>
2024-04-25 17:43:36 +0530

    added reset code

--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -2,11 +2,11 @@
     <div class="flex justify-center min-h-screen pt-20 flowbit-lg:pt-0">
       <section class="dark:bg-gray-900 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-8">
         <AppLogo class="justify-center mb-6"/>
-        <h3 class="text-md font-semibold text-gray-600 mb-4 mt-6 text-center">Reset Your Password</h3>
+        <h3 class="text-md font-semibold text-gray-600 mb-4 mt-6 text-center">Reset your Password</h3>
         <span class="text-sm text-gray-500 mt-3 px-4 md:px-4 flex justify-center text-center">No worries, we will mail you a like to your recovery email address to reset your password.</span>
         <div class="mt-6">
-          <FormInput id="Email" label="Email" name="Email" type="text" placeholder="Recovery email ID" />
-          <Button :text="'Reset'" @click="reset" class="mt-3"/>
+          <FormInput id="Email" label="Email" name="Email" v-model="v$.reset.email.$model" :errors="v$.reset.email.$errors" type="text" placeholder="Recovery email ID" />
+          <Button :text="'Send Code'" class="mt-3" @click="resetSendLink()" />
           <span class="text-xs text-gray-500 mt-5 flex">Did not receive code? <a href="#" class="underline decoration-gray-400">Send again</a></span>
         </div>
       </section>
@@ -16,12 +16,38 @@
   
   <script setup lang="ts">
   import AppFooter from '@/layouts/AppFooter'
-  const router = useRouter()
+  import { useAuth } from "@/stores/auth";
+  const auth = useAuth()
+  const { $toast } = useNuxtApp()
+  import { useVuelidate } from "@vuelidate/core";
+  import { required, email, helpers } from "@vuelidate/validators";   
   definePageMeta({
     layout: 'loginLayout',
   });
-  const reset = (() => {
-    router.push({path: 'update-password', query: { token: 'a7RbXp2YvS6wKqL9j3CmNt8PzF4xVdG7u5HvRf9TcJ2bWqNp' }})
+  const reset = ref({
+    email: null,
+    redirect_url: window?.location?.origin + '/rest-password' 
   })
+  const rules = {
+    reset:{
+      email: {
+        required: helpers.withMessage("The Email field is required", required),
+        email: helpers.withMessage("Please Enter a valid Email Address", email),
+      },
+    }
+  }
+  const v$ = useVuelidate(rules, {reset})
+  const resetSendLink = async () => {
+    const result = await v$.value.$validate()
+    if (result) {  
+      auth.resetPassword(reset.value).then((resp:any) => {
+        if(resp?.success) {   
+            $toast('success', 'Reset Password Send Link Successfully', { duration: 10000 })
+        }
+      }).catch((error) => {
+        console.log("Error:", error);                  
+      })
+    }
+  }
   </script>
   
