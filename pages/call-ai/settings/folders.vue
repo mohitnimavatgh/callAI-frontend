ad871c52564129da7563585a3e40ee21b7b44f09
Mohit Nimavat <mohit@gmail.com>
2024-05-01 16:23:57 +0530

    update folder design

--- pages/call-ai/settings/folders.vue
+++ pages/call-ai/settings/folders.vue
@@ -103,20 +103,35 @@
       <div class="mt-3 flex justify-end">
         <FormInput type="text" icon="fas fa-search" :placeholder="`Search Folders`" v-model="search" @input="handleSearch" class="w-56 "/>
       </div>
-      <div class="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">        
-        <div v-for="folderItem in foldersLists?.data" :key="folderItem" class="relative col-span-full sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
-          <div class="relative w-full mt-2">            
-            <img src="@/assets/image/folder.svg" alt="Folder" class="w-full h-auto">            
-            <div class="absolute top-10 right-4 flex">
-              <i class="text-sm text-gray-400 mr-2" :class="`fas fa-lock${folderItem.access_type == 'public'?'-open':''}`"></i>
-              <i class="text-sm text-gray-400 fas fa-pen cursor-pointer" @click="edit(folderItem)"></i>
-            </div>            
-            <div class="absolute bottom-4 left-4">
-              <p class="font-medium text-gray-500 text-sm">{{ folderItem.name }}</p>
-              <p class="font-medium text-gray-400 text-xs">Meetings: {{ folderItem.meeting_count }}</p>
+      <div class="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-4 bg-white dark:bg-gray-800 p-5 py-10 rounded-[20px]">        
+        <div v-for="folderItem in foldersLists?.data" :key="folderItem" class="relative col-span-full sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1 bg-white dark:bg-gray-700">
+          <div class="flex justify-between border border-gray-100 px-3 py-1 rounded-lg items-center dark:border-none">
+            <div class="flex items-center">
+              <div class="bg-primary-50 dark:bg-gray-800 w-12 h-12 rounded-md">
+                <div class="flex w-full h-full items-center text-center justify-center">                
+                  <i class="fas fa-folder-open text-md text-primary-400 dark:text-primary-500"></i>
+                </div>
+              </div>
+              <div class="ml-3 p-5">
+                <p class="text-xs font-semibold text-gray-500">{{ folderItem.name }}</p>
+                <p class="text-xs text-gray-400">Meetings: {{ folderItem.meeting_count }}</p>
+              </div>
             </div>
-          </div>
-        </div>   
+            <div class="flex items-center ">
+              <i class="text-xs text-gray-300 mr-2" :class="`fas fa-lock${folderItem.access_type == 'public'?'-open':''}`"></i> 
+              <div  @mouseleave="folderItem.active = false" class="cursor-pointer">
+                <i class="fas fa-ellipsis text-gray-300" @mouseover="folderItem.active = true" @click="folderItem.active = true" ></i>
+                <div v-if="folderItem.active" class="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-20 dark:bg-gray-700 dark:divide-gray-600">
+                  <ul class="py-1 text-sm text-gray-700 dark:text-gray-200">
+                    <li>
+                      <a @click="edit(folderItem)" class="text-gray-400 text-xs block px-2 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><i class="fas fa-pen text-gray-400 mr-2"></i>Edit</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>  
+        </div>       
       </div>
       <div class="flex justify-center">
         <Pagination v-if="foldersLists && foldersLists.total && foldersLists.per_page && foldersLists.total > foldersLists.per_page" class="mt-4 flex justify-end" :totalRecords="foldersLists.total" :currentPage="folderParams.page" :recordsPerPage="foldersLists.per_page" @pageChange="folderPageChange"/>
