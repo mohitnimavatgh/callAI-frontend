8f68744dc4f01f6aad35ff80ef1e8b2807fd0891
Mohit Nimavat <mohit.nimavat@bigsteptech.com>
2024-05-08 11:52:09 +0530

    fixing dropdown

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -35,7 +35,7 @@ onMounted(() => {
 <template>
   <div>
     <label class="block px-5 py-4 text-md font-medium text-gray-700 dark:text-white bg-gray-200 border "><i :class="icon" class="mr-2"></i>{{title}}</label>
-    <div v-for="(accordion, index) in accordions" :key="index" :id="`accordion-${index}`" :data-accordion="accordion.isOpen ? 'open' : 'closed'">
+    <!-- <div v-for="(accordion, index) in accordions" :key="index" :id="`accordion-${index}`" :data-accordion="accordion.isOpen ? 'open' : 'closed'">
       <h2 :id="`accordion-${index}-heading`">
         <button type="button" class="text-start bg-white-100 flex items-center justify-between w-full px-5 py-3 font-medium rtl:text-right text-gray-700 border border-b-0 border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-200 gap-3" :data-accordion-target="`#accordion-${index}-body`" :aria-expanded="accordion.isOpen" :aria-controls="`accordion-${index}-body`" @click="toggleAccordion(index)">
           <span class="flex items-center text-sm">{{ accordion.title }}</span>
@@ -45,6 +45,28 @@ onMounted(() => {
       <div :id="`accordion-${index}-body`" :class="{ hidden: !accordion.isOpen }" :aria-labelledby="`accordion-${index}-heading`">
         <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
           <p class="text-sm mb-2 text-gray-500 dark:text-gray-400 leading-6">{{ accordion.description }}</p>
+        </div>
+      </div>
+    </div> -->
+    <div v-for="(accordion, index) in accordions" :key="index">
+      <div data-accordion="collapse">
+        <h2 :id="`accordion-collapse-heading-${index}`">
+          <button type="button"
+            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+            :data-accordion-target="`#accordion-collapse-body-${index}`" aria-expanded="true"
+            :aria-controls="`accordion-collapse-body-${index}`">
+            <span>{{ accordion.title }}</span>
+            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M9 5 5 1 1 5" />
+            </svg>
+          </button>
+        </h2>
+        <div :id="`accordion-collapse-body-${index}`" class="hidden" :aria-labelledby="`accordion-collapse-heading-${index}`">
+          <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+            <p class="mb-2 text-gray-500 dark:text-gray-400">{{ accordion.description }}</p>
+          </div>
         </div>
       </div>
     </div>
--- components/DropDown.vue
+++ components/DropDown.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="h-full">
       <button @click="toggleDropdown" :class="{'inline-flex items-center text-gray-500 rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700': true, 'focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium border border-gray-300': !image }"  type="button">
         <div v-if="image"> 
             <span class="sr-only">Open user menu</span>
@@ -11,7 +11,7 @@
             <i :class="{ 'transform rotate-180': dropdownOpen }" class="ms-2.5 transition duration-75  fas fa-chevron-down text-xs" ></i>
         </div>
       </button>
-      <div v-show="dropdownOpen" class="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+      <div v-show="dropdownOpen" class="absolute h-60 overflow-y-auto z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
         <ul class="py-1 text-sm text-gray-700 dark:text-gray-200">
           <li v-for="(action, index) in actions" :key="index" @click="$emit('select', action);">
             <a class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ action.name }}</a>
